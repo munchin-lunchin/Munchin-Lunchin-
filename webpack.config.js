9006bd83b9c02ca50e769d38a9018fa90f38cf75
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-05-17 20:01:33 -0400

    Webpack setup complete

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react'],
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/dist/',
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/messages': 'http://localhost:3000',
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  }
+};
