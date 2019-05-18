8b161d4dde7eb4138322d12d0287578205304ad4
Jarred <jackhajb@gmail.com>
2019-05-18 13:35:46 -0400

    Updated App.jsx to user react router

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LoginContainer from './LoginContainer'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -8,10 +9,13 @@ class App extends Component {
 
   render () {
     return (
-      <div>
-        Hello World!
-        <LoginContainer />
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={LoginContainer} />
+          {/* <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} /> */}
+        </div>
+      </Router>
     );
   }
 
