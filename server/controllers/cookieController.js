9df2d5e07627f51a836d542f1059b056c309a62f
Joe <20054730+joepav@users.noreply.github.com>
2019-05-19 20:16:22 -0400

    whoops should have been addedt o last commit

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,7 +1,9 @@
 const cookieController = {};
 
 cookieController.setCookie = (req, res, next) => {
-  res.cookie('userId', res.locals.id, { maxAge: 5000, httpOnly: true });
+  // httpOnly is set to false bc it's necessary for the authentication function
+  // would be great to adjust but good enough for now
+  res.cookie('userId', res.locals.id, { maxAge: 50000, httpOnly: false });
   res.json({ authenticated: true });
 }
 
