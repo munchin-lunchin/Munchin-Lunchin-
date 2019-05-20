43150138f018f701b4e5a7b6ec7f81db3e687d95
Bryan Fong <bryanfong.dev@gmail.com>
2019-05-20 16:44:01 -0400

    css file is now linked

--- client/components/RestaurantComponent.jsx
+++ client/components/RestaurantComponent.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 
-function RestaurantComponent ({ name, rating, displayAddress, price, reviewCount }) {
- return (
-  <div>
+function RestaurantComponent({ name, rating, displayAddress, price, reviewCount }) {
+  return (
+    <div className="history">
 
-    <strong>Name: </strong>{name}
-    <br/>
-    <strong>Rating: </strong>{rating}
-    <br/>
-    <strong>Review Count: </strong>{reviewCount}
-    <br/>
-    <strong>Price: </strong>{price}
-    <br/>
-    <strong>Address: </strong>{displayAddress}
-  </div>
- )
+      <strong>Name: </strong>{name}
+      <br />
+      <strong>Rating: </strong>{rating}
+      <br />
+      <strong>Review Count: </strong>{reviewCount}
+      <br />
+      <strong>Price: </strong>{price}
+      <br />
+      <strong>Address: </strong>{displayAddress}
+    </div>
+  )
 }
 
 export default RestaurantComponent; 
--- server/schemas/gqlSchema.js
+++ server/schemas/gqlSchema.js
@@ -1,4 +1,4 @@
-const { 
+const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
@@ -53,7 +53,7 @@ const UserType = new GraphQLObjectType({
         return pool
           .query(getRestaurants)
           .then(restaurants => {
-            console.log('The restaurants from the db are: \n', restaurants.rows);
+            // console.log('The restaurants from the db are: \n', restaurants.rows);
             return restaurants.rows;
           })
           .catch(err => console.error('Error during "select restaurants" GraphQL UserType\n', err));
@@ -78,7 +78,7 @@ const RestaurantType = new GraphQLObjectType({
     longitude: { type: GraphQLFloat },
     users: {
       type: new GraphQLList(UserType),
-      resolve(parent, args) { 
+      resolve(parent, args) {
         const getUsers = `
           SELECT u.* 
           FROM likes l INNER JOIN users u ON l.user_id = u._id 
@@ -87,7 +87,7 @@ const RestaurantType = new GraphQLObjectType({
         return pool
           .query(getUsers)
           .then(users => {
-            console.log('The users from the db are: \n', users.rows);
+            // console.log('The users from the db are: \n', users.rows);
             return users.rows;
           })
           .catch(err => console.error('Error during "select users" GraphQL RestaurantType\n', err));
@@ -156,7 +156,7 @@ const Mutation = new GraphQLObjectType({
   fields: {
     addLike: {
       type: LikeType,
-      args: { 
+      args: {
         user_id: { type: GraphQLInt },
         rest_id: { type: GraphQLInt }
       },
@@ -177,7 +177,7 @@ const Mutation = new GraphQLObjectType({
 
     deleteLike: {
       type: LikeType,
-      args: { 
+      args: {
         user_id: { type: GraphQLInt },
         rest_id: { type: GraphQLInt }
       },
@@ -201,7 +201,7 @@ const Mutation = new GraphQLObjectType({
 
     addUser: {
       type: UserType,
-      args: { 
+      args: {
         username: { type: GraphQLString },
         password: { type: GraphQLString }
       },
@@ -236,7 +236,7 @@ const Mutation = new GraphQLObjectType({
     // }
     addRestaurant: {
       type: RestaurantType,
-      args: { 
+      args: {
         rating: { type: GraphQLInt },
         reviewCount: { type: GraphQLInt },
         yelpID: { type: GraphQLString },
@@ -297,7 +297,7 @@ const Mutation = new GraphQLObjectType({
     deleteRestaurant: {
       type: RestaurantType,
       args: {
-        yelpID: { type: GraphQLString }        
+        yelpID: { type: GraphQLString }
       },
       resolve(parent, { yelpID }) {
         let restaurant;
