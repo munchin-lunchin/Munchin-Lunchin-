a8c123b058209a99bcb54fc150beda5be6e449e4
Bryan Fong <bryanfong.dev@gmail.com>
2019-05-20 14:49:59 -0400

    added controller for searching and adding restaurant to db -- need to add restaurant to likes table

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -2,11 +2,33 @@ const pool = require('../database/psqlDb.js');
 
 const dbController = {};
 
+dbController.searchForRestaurant = (req, res, next) => {
+  const { url } = req.body.businesses[0];
+  // console.log(url);
+  const find = `SELECT _id FROM restaurant WHERE URL='${url}'`
+  pool.query(find)
+    .then(result => {
+      console.log('searching database for restaurant')
+      if (result.rows.length) {
+        res.locals.rest_id = result.rows[0]._id
+      }
+      res.locals.rest_id ? console.log('found restaurant in db') : console.log('did not find restaurant in db');
+      return next();
+    })
+    .catch(err => {
+      console.log('ERROR');
+      res.status(400).send();
+    });
+}
+
 dbController.addRestaurant = (req, res, next) => {
+  if (res.locals.rest_id) return next();
   const { id, name, rating, image_url, review_count, url, price } = req.body.businesses[0];
   const { latitude, longitude } = req.body.businesses[0].coordinates;
   const { display_address } = req.body.businesses[0].location;
   const displayAddress = display_address.join(" ");
+
+  console.log(`adding ${name} to db! `)
 
   //create query string
   const add = {
@@ -17,18 +39,28 @@ dbController.addRestaurant = (req, res, next) => {
   //add query to database
   pool.query(add)
     .then(result => {
-      console.log('successfully added to database');
-      res.status(200).send(req.body);
+      console.log(`successfully added ${name} to db`);
+      return next();
     })
-    .catch(err => {
-      if (err.routine = '_bt_check_unique') return res.status(200).send(req.body);
-      else return res.status(400).send();
-    })
+    .catch(err => res.status(400).send());
 }
 
-//need to create !! 
-//next -- add user/restaurant to likes table
+dbController.addToLikeTable = (req, res) => {
+  console.log('add to the like table now!')
+  res.status(200).send(req.body);
+}
 
+
+
+// pool.query(add)
+// .then(result => {
+//   console.log('successfully added to database');
+//   res.status(200).send(req.body);
+// })
+// .catch(err => {
+//   if (err.routine = '_bt_check_unique') return res.status(200).send(req.body);
+//   else return 
+// })
 
 
 
