bb36cc667ca1544dd8bd027517aa6335bb49445d
Conor Sexton <sextonc@me.com>
2019-05-22 14:55:23 -0400

    Delete keys.js
    Don’t need the keys file at all

--- server/keys.js
+++
@@ -1,9 +0,0 @@
-exports.database = {
-  dev: process.env.DEV_DB,
-  secret: process.env.SPOTIFY_SECRET
-};
-
-exports.yelp = {
-  CLIENT_ID: process.env.YELP_ClientID,
-  API_KEY: process.env.YELP_API_Key
-}
