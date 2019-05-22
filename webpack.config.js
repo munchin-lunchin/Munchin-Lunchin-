406cd0e340b9d7854cbd287e819eebf9f3c4a893
saylestyler <saylestyler@gmail.com>
2019-05-22 19:20:06 -0400

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './client/index.js',
-  // target: 'node',
   module: {
     rules: [
       {
@@ -37,3 +36,4 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
