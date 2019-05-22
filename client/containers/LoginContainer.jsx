6566af4a1c5a4c20b82f0e8ac5837d8b7eb2f3af
Conor Sexton <sextonc@me.com>
2019-05-21 22:25:26 -0400

    Initialized styled components for form and form labels

--- client/containers/LoginContainer.jsx
+++ client/containers/LoginContainer.jsx
@@ -6,16 +6,22 @@ import styled from 'styled-components';
 
 function LoginContainer() {
   const [redirect, setRedirect] = useState(false);
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+  `
   const Input = styled.input`
     color: #3C3832;
     border: 1px solid #A69E91;
-    box-shadow: inset 0 1px 5px 1px rgba(0,0,0,0.15);
+    box-shadow: inset 0 1px 3px 1px rgba(0,0,0,0.15);
     border-radius: 5px;
-    font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    // font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
     font-weight: 500;
     font-size: 1em;
     line-height: 1em;
     padding: 0.75em 1em;
+    width: 18em;
     transition: all 0.2s ease-in-out;
     &:focus {
       outline: 0;
@@ -23,10 +29,15 @@ function LoginContainer() {
                         0 0 0 2px #F3C276;
     }
   `
+  const FormLabel = styled.label`
+    color: #918D88;
+  `
   return (
-    <form id="loginContainer">
+    <Form id="loginContainer">
       {/* <input type='text' name='username'  className='loginInputs' required /> */}
-      <Input type='text' name='username'  className='loginInputs' required />
+      <FormLabel htmlFor="username">Username</FormLabel>
+      <Input type='text' name='username' className='loginInputs' required />
+      <FormLabel htmlFor="password">Password</FormLabel>
       <Input type='password' name='password' className='loginInputs' required />
       <button type='submit' id='login' onClick={() => {
         const data = {
@@ -52,7 +63,7 @@ function LoginContainer() {
           .catch(error => console.error('Error:', error));
       }}> Login! </button>
       {redirect && <Redirect to='/main' />}
-    </form>
+    </Form>
   )
 }
 
