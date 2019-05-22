a575df7ea0de5d2e6c048d126c8d4f5ac486a676
Conor Sexton <sextonc@me.com>
2019-05-22 14:54:53 -0400

    Update keys.js
    Get rid of Twitter API keys—we don’t need them

--- server/keys.js
+++ server/keys.js
@@ -1,10 +1,3 @@
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-
 exports.database = {
   dev: process.env.DEV_DB,
   secret: process.env.SPOTIFY_SECRET
