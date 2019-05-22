91959b0f7f4c4bb4896ef36356ac4623638448f9
Conor Sexton <sextonc@me.com>
2019-05-21 22:18:56 -0400

    Initial form input styles

--- client/containers/LoginContainer.jsx
+++ client/containers/LoginContainer.jsx
@@ -1,15 +1,33 @@
 // URL for fetch req hardcoded in!
 import React, { useState } from 'react';
-import { Redirect } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
+import styled from 'styled-components';
 // import isAuthenticated from "./../services/authenticate";
 
 function LoginContainer() {
   const [redirect, setRedirect] = useState(false);
-
+  const Input = styled.input`
+    color: #3C3832;
+    border: 1px solid #A69E91;
+    box-shadow: inset 0 1px 5px 1px rgba(0,0,0,0.15);
+    border-radius: 5px;
+    font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 500;
+    font-size: 1em;
+    line-height: 1em;
+    padding: 0.75em 1em;
+    transition: all 0.2s ease-in-out;
+    &:focus {
+      outline: 0;
+      box-shadow: inset 0 1px 5px 1px rgba(0,0,0,0.15),
+                        0 0 0 2px #F3C276;
+    }
+  `
   return (
     <form id="loginContainer">
-      <input type='text' name='username'  className='loginInputs' required />
-      <input type='password' name='password' className='loginInputs' required />
+      {/* <input type='text' name='username'  className='loginInputs' required /> */}
+      <Input type='text' name='username'  className='loginInputs' required />
+      <Input type='password' name='password' className='loginInputs' required />
       <button type='submit' id='login' onClick={() => {
         const data = {
           username: document.querySelector('#username').value,
