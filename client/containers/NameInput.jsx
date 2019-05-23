34435631f8c99a2aa0802ba3f4cf6deb564b2617
Bryan Fong <bryanfong.dev@gmail.com>
2019-05-23 17:22:32 -0400

    added styling to fonts

--- client/containers/NameInput.jsx
+++ client/containers/NameInput.jsx
@@ -4,7 +4,10 @@ const Input = styled.input`
   width: auto;
   border: none;
   outline: none;
-  float: left;
+  font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding-left: 1em;
+  line-height: 1em;
   `;
 
 export default Input;
--- client/containers/ZipInput.jsx
+++ client/containers/ZipInput.jsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 const Input = styled.input`
-  width: 1em;
+  width: 5em;
   border: none;
   outline: none;
+  font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  padding-left: 1em;
+  line-height: 1em;
   `;
 
 export default Input;
