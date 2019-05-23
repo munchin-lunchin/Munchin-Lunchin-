835b08450bbc978a4107d518c30880a5f95aa2e6
Rodolfoguzman25 <rodolfoguzman147@gmail.com>
2019-05-23 14:57:08 -0400

    imported styles into header Component

--- client/components/HeaderComponent.jsx
+++ client/components/HeaderComponent.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from './Logo';
 import { Link } from "react-router-dom";
 
 const HeaderComponent = () => (
-  <div id="headerComp">
-    <h1>Munchin' Lunchin'</h1>
-    <Link to="/main">
-      <button>Go to main page</button>
-    </Link>
-    <Link to="/">
-      <button>Go to login ('/') page</button>
-    </Link>
-  </div>
-)
+      <div id="headerComp">
+        <h1>Munchin' Lunchin'</h1>
+        <Link to="/main">
+          <button>Go to main page</button>
+        </Link>
+        <Link to="/">
+          <button>Go to login ('/') page</button>
+        </Link>
+      </div>
+    )
 
 export default HeaderComponent;
