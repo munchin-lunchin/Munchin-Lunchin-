60d5627b48c9c758ba151c18e2cd5cc3ed85e892
Conor Sexton <sextonc@me.com>
2019-05-23 00:02:06 -0400

    Style tweaks to search results container and adds heart to results

--- client/components/RestaurantSearchResult.jsx
+++ client/components/RestaurantSearchResult.jsx
@@ -13,7 +13,7 @@ const RestaurantSearchResult = (props) => {
     display: `flex`,
     flexDirection: `column`,
     borderBottom: `1px solid #F1ECE6`,
-    padding: `0.5em 1em 0.75em 1em`,
+    padding: `0.5em 1em 0.75em 0`,
   }}>
     <Name>{props.data.name}</Name>
     <LocationContainer>
@@ -32,8 +32,16 @@ const RestaurantSearchResult = (props) => {
       </svg>
       <Address dangerouslySetInnerHTML={{__html: `${props.data.location.display_address[0]}
       <br/>${props.data.location.display_address[1]}`}} />
-      {/* </Address> */}
     </LocationContainer>
+    <svg width="35px" height="31px" viewBox="0 0 35 31">
+    <g id="Main-App" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="Desktop" transform="translate(-837.000000, -348.000000)" fill="#F57E7E" fill-rule="nonzero">
+        <g id="heartempty" transform="translate(837.000000, 348.000000)">
+          <path d="M17.5,31 C13.8423587,31 0.000666654284,22.1233614 0.000666654284,10.1347455 C-0.0621934833,4.61079753 4.32826288,0.0775504293 9.81618227,0 C12.8293144,0.0126223264 15.6667421,1.4291946 17.4999992,3.83610909 C19.3332008,1.42912671 22.1706639,0.0125367586 25.1838161,1.25152414e-15 C30.6717684,0.0774734053 35.0622692,4.61076638 34.9993317,10.1347455 C34.9993317,22.1233614 21.1577797,31 17.5,31 Z M25.1838161,2.81818182 C20.6463092,2.81818182 19.1069979,7.04545455 17.4999992,7.04545455 C15.8930005,7.04545455 14.3536892,2.81818182 9.81611227,2.81818182 C5.87396803,2.89430822 2.73625466,6.16664919 2.80055986,10.1347455 C2.80055986,20.773875 15.668449,28.1598364 17.4999992,28.1818182 C19.3314793,28.1598364 32.1994385,20.773875 32.1994385,10.1347455 C32.2637434,6.16662221 29.1259879,2.89426992 25.1838161,2.81818182 L25.1838161,2.81818182 Z" id="Shape"></path>
+        </g>
+      </g>
+    </g>
+    </svg>
   </article>
     // <div className="searchResult">
     //   <h4>{props.data.name}</h4>
