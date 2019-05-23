6b5511ca92f98f72fee6cce9e6bfe3d8f74ad497
Conor Sexton <sextonc@me.com>
2019-05-23 17:03:39 -0400

    Modified restaurant card styles and modularized inner components

---
+++ client/components/Price.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Price = styled.p`
+  color: #918D88;
+`;
+
+export default Price;
---
+++ client/components/RestaurantData.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const RestaurantData = styled.section`
+font-family: 'Avenir Next', 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export default RestaurantData;
