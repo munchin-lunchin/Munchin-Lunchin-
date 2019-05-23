57c7585bb56aa80f173fb8871e78ad511f2aadba
Rodolfoguzman25 <rodolfoguzman147@gmail.com>
2019-05-23 15:03:05 -0400

    created a layout for the styled header

---
+++ client/components/StyledHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import HeaderComponent from './HeaderComponent';
+
+const StyledHeader = styled(HeaderComponent)`
+    display: flex;
+    justify-content: space-around;
+`;
+
+export default StyledHeader;
