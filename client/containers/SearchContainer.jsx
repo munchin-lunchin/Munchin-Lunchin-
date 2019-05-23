9af110311eaeb4462711fc69497dde8b89e35d9f
Bryan Fong <bryanfong.dev@gmail.com>
2019-05-23 15:59:39 -0400

    changed input name

--- client/containers/SearchContainer.jsx
+++ client/containers/SearchContainer.jsx
@@ -3,14 +3,14 @@ import RestaurantSearchResult from './../components/RestaurantSearchResult';
 
 const SearchContainer = () => {
   const [restaurantList, setRestaurantList] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [zip, setZip] = useState('');
   const [disabled, setDisabled] = useState(true);
 
   const handleChange = (event) => {
     const { target } = event;
-    if (target.name === 'username') setUsername(target.value);
-    else if (target.name === 'password') setPassword(target.value);
+    if (target.name === 'name') setName(target.value);
+    else if (target.name === 'zip') setZip(target.value);
     else (console.error('Unrecognized form field'));
     canSubmit();
   }
@@ -23,8 +23,8 @@ const SearchContainer = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        'name': username,
-        'zip': password,
+        'name': name,
+        'zip': zip,
       }),
     }
     console.log(JSON.stringify(payload));
@@ -63,8 +63,8 @@ const SearchContainer = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type='text' name='username' className='loginInputs' onChange={handleChange} onBlur={handleChange} required autoComplete="off"/>
-        <input type='password' name='password' className='loginInputs' onChange={handleChange} onBlur={handleChange} required />
+        <input type='text' name='name' className='loginInputs' onChange={handleChange} onBlur={handleChange} required autoComplete="off" />
+        <input type='text' name='zip' className='loginInputs' onChange={handleChange} onBlur={handleChange} required />
         <button type='submit' id='login' disabled={disabled}>Log In</button>
       </form>
       <div id="searchContainer">
