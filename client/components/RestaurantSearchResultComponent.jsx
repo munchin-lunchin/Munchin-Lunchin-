46959ec8872e1687498787b46fa1898d642eb074
Conor Sexton <sextonc@me.com>
2019-05-22 20:09:52 -0400

    Set up component imports and basic structure of search result

--- client/components/RestaurantSearchResultComponent.jsx
+++ client/components/RestaurantSearchResultComponent.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 const RestaurantSearchResultComponent = (props) => {
 
   return (
-    <div className="searchResult">
-      <h4>{props.data.name}</h4>
-      <h6>{props.data.location.display_address[0]}<br />
-        {props.data.location.display_address[1]}</h6>
-      <button onClick={() => { props.likeRestaurant(props.data) }}> Like </button>
-    </div>);
+  <Result>
+    <Name>{props.name}</Name>
+    <LocationContainer>
+      <Icon icon='pin' />
+      <Address>{props.address}</Address>
+    </LocationContainer>
+    <Icon icon='like' onClick={handleLike} />
+  </Result> 
+    // <div className="searchResult">
+    //   <h4>{props.data.name}</h4>
+    //   <h6>{props.data.location.display_address[0]}<br />
+    //     {props.data.location.display_address[1]}</h6>
+    //   <button onClick={() => { props.likeRestaurant(props.data) }}> Like </button>
+    // </div>
+  );
 }
 
 export default RestaurantSearchResultComponent;
