28f553c1b8456c1189f6aedb888b716efe62e250
Alice Wong <alicewong@Alices-MacBook-Pro.local>
2019-05-23 14:08:23 -0400

    Oops :)

--- client/containers/SearchContainer.jsx
+++ client/containers/SearchContainer.jsx
@@ -4,14 +4,14 @@ import { gql } from 'apollo-boost';
 import Hit from '../components/HitComponent';
 import algoliasearch from 'algoliasearch';
 import {
-	graphql,
-	compose,
-	ApolloConsumer
+  graphql,
+  compose,
+  ApolloConsumer
 } from 'react-apollo';
 import {
-	InstantSearch,
-	Hits,
-	SearchBox,
+  InstantSearch,
+  Hits,
+  SearchBox,
 } from 'react-instantsearch-dom';
 
 const client = algoliasearch('54V98YN658', 'd4fd1c2bd8718edd438f6fc30b0e8c30');
@@ -19,7 +19,7 @@ const searchClient = algoliasearch('54V98YN658', '488d342c46cad2e1015749231a63ea
 const index = client.initIndex('yelp')
 
 const SearchRestaurantsQuery = gql`
-  query yelp( 
+  query yelp(
         $name: String!,
         $zipcode: Int!
       ){
@@ -39,54 +39,93 @@ const SearchRestaurantsQuery = gql`
 
 const query1 = SearchRestaurantsQuery;
 
+const AddRestaurantMutation = gql`
+  mutation {
+    addRestaurant(
+        rating: $rating,
+        review_count: $review_count,
+        yelp_id: $yelp_id,
+        name: $name,
+        display_address: $display_address,
+        image_url: $image_url,
+        url: $url,
+        price: $price,
+        latitude: $latitude,
+        longitude: $longitude
+      ) {
+      rating
+      review_count
+      yelp_id
+      name
+      display_address
+      image_url
+      url
+      price
+      latitude
+      longitude
+    }
+  }
+`
+
 const SearchContainer = () => {
-	const [restaurantList, setRestaurantList] = useState([]);
-	const [zipcode, setZipCode] = useState('');
-	const [restName, setRestName] = useState('');
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [zipcode, setZipCode] = useState('');
+  const [restName, setRestName] = useState('');
 
-	const searchResultComponents = [];
-	for (const restaurant of restaurantList) {
-		searchResultComponents.push(
-			<RestaurantSearchResultComponent
-				key={restaurant.id}
-				data={restaurant}
-				addRestaurantMutation={AddRestaurantMutation}
-			/>
-		)
-	};
+  const searchResultComponents = [];
+  for (const restaurant of restaurantList) {
+    searchResultComponents.push(
+      <RestaurantSearchResultComponent
+        key={restaurant.id}
+        data={restaurant}
+        addRestaurantMutation={AddRestaurantMutation}
+      />
+    )
+  };
 
-	return (
-		<ApolloConsumer>
-			{client => (
-				<div>
-					<div>
-						<h1> Search</h1>
-						Restaurant Name: <input id="whereYouAteYoFoodsInput" onChange={(e) => setRestName(e.target.value)}></input>
-						Zipcode: <input id="zipcodeOfWhereYouEatYoFoodsInput" onChange={(e) => setZipCode(e.target.value)}></input>
-						<button id="yelpSearchButton" onClick={async () => {
-							const { data } = await client.query({
-								query: query1,
-								variables: { name: restName, zipcode: parseInt(zipcode) }
-							});
-							setRestaurantList(data.yelp);
-							console.log('data is ', data.yelp);
-						}}> Search for restaurants </button>
-						<div id="searchContainer">
-							{searchResultComponents}
-						</div>
-						<h2>recently viewed</h2>
-						<InstantSearch indexName="yelp" searchClient={searchClient}>
-							<div>
-								<SearchBox />
-								<Hits hitComponent={Hit} />
-							</div>
-						</InstantSearch>
-					</div>
+  return (
+    <ApolloConsumer>
+      {client => (
+        <div>
+          <h1> Search</h1>
+          Restaurant Name: <input id="whereYouAteYoFoodsInput" onChange={(e) => setRestName(e.target.value)}></input>
+          Zipcode: <input id="zipcodeOfWhereYouEatYoFoodsInput" onChange={(e) => setZipCode(e.target.value)}></input>
 
-				</div>
-			)}
-		</ApolloConsumer>
-	)
+          <button
+            id="yelpSearchButton"
+            onClick={async () => {
+              const { data } = await client.query({
+                query: query1,
+                variables: { name: restName, zipcode: parseInt(zipcode) }
+              });
+              setRestaurantList(data.yelp);
+              index.addObjects(data.yelp, (err) => {
+                if (err) return console.error(err)
+              })
+            }}>
+            Search for restaurants
+          </button>
+
+          <div id="searchContainer">
+            {searchResultComponents}
+          </div>
+
+          <hr />
+
+          <div>
+            <h2>recently viewed</h2>
+            <InstantSearch indexName="yelp" searchClient={searchClient}>
+              <div>
+                <SearchBox />
+                <Hits hitComponent={Hit} />
+              </div>
+            </InstantSearch>
+          </div>
+
+        </div>
+      )}
+    </ApolloConsumer>
+  )
 };
 
 export default SearchContainer;
